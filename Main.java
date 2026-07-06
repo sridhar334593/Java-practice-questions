ec0acf32d00afb19f0fdcab5e01690b317a005a7
sridhar <sridharahdirs33@gmail.com>
2026-07-06 08:45:54 +0530

    variables in java(easy lvl)

---
+++ Main.java
@@ -0,0 +1,89 @@
+public class Main {
+  public static void main(String[] args) {
+    solveVariablesQ1();
+    solveVariablesQ2();
+    solveVariablesQ3();
+    solveVariablesQ4();
+    solveVariablesQ5();
+    solveVariablesQ6();
+    solveVariablesQ7();
+    solveVariablesQ8();
+    solveVariablesQ9();
+    solveVariablesQ10();
+  }
+
+ 
+
+  public static void solveVariablesQ1() {
+    int age = 25;
+    System.out.println("Age: " + age);
+  }
+
+  public static void solveVariablesQ2() {
+   double height = 5.5;
+    System.out.println("height: " + height + " m");
+  }
+
+  public static void solveVariablesQ3() {
+    String name = "Sridhar";
+    System.out.println("First letter of name: " + name.charAt(0));
+  }
+  // store 1st letter and printit
+  //char firstletter = 's';
+  //System.out.println("First letter of name: " + firstletter);
+
+  public static void solveVariablesQ4() {
+    boolean pass = true;
+    System.out.println("Has passed: " + pass);
+  }
+
+  public static void solveVariablesQ5() {
+    String name = "Sridhar";
+    int Age = 20;
+    double CGPA = 8.4;
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + Age);
+    System.out.println("CGPA: " + CGPA);
+  }
+
+  public static void solveVariablesQ6() {
+   int a = 15;
+   int b = 25;
+   int c = a + b;
+   System.out.println("Sum: " + c);
+  }
+
+  public static void solveVariablesQ7() {
+    double pi = 3.14159;
+    System.out.println("Value of pi: " + pi);
+  }
+
+  public static void solveVariablesQ8() {
+   double price = 65000.75;
+   System.out.println("Price of laptop: $" + price);
+  }
+
+  public static void solveVariablesQ9() {
+    byte a =1;
+    short b = 20;
+    int c = 3000;
+    long d = 40000L;
+    float e = 5.00000f;
+    double f = 6.0000000;
+    char g = 'A';
+    boolean h = true;
+    System.out.println("Byte: " + a + ", Short: " + b + ", Int: " + c + ", Long: " + d + ", Float: " + e + ", Double: " + f + ", Char: " + g + ", Boolean: " + h   );
+
+  }
+
+  public static void solveVariablesQ10() {
+    int a =10, b = 20, c = 30, d = 40, e = 50;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+  }
+}
+
+
