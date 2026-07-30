ab7badbe51f99a136d797b560b5cb6295c10c3c1
sridhar <sridharahdirs33@gmail.com>
2026-07-30 13:56:27 +0530

    loop problem

---
+++ LoopProblem.java
@@ -0,0 +1,45 @@
+public class LoopProblem{
+  public static void main(String[] args) {
+    int i,j;
+
+   /*  for (i =1 ;i <= 10 ; i++){         // for( int i = 1 ; i <= 10 ; i++)
+        System.out.println(i);
+       }
+
+    */
+
+    System.out.println();
+
+    /*  for ( i = 10 ; i >= 1 ; i--){
+         System.out.println(i);
+        }
+        
+    */
+
+    /*  for(i = 1 ; i <= 20 ; i++){
+         System.out.println(i);
+        }
+    */
+
+    // odd numbers
+    
+    /*  for(i=1;i<=19;i+=2){
+         System.out.println(i);
+        }
+    */
+
+    /*   for(i=1;i<=5;i++){
+          System.out.print("*");
+         }
+
+    */
+
+    for(i=1;i<=3;i++){
+      for(j=1;j<=5;j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    
+  }
+}
